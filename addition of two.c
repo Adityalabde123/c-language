f083df75c638c29f9162e3f330646669afd94246
Adityalabde123 <136427111+Adityalabde123@users.noreply.github.com>
2023-07-04 12:09:47 -0400

    Add files via upload

---
+++ addition of two.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    prinf("enter number");
+    scanf("%d",&a);
+    printf("enter number");
+    scanf("%d",&b);
+    c=a+b;
+    printf("addition=%d",c);
+}
